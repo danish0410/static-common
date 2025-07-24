4b8220b40c7903d20e8bd9b2b0f2b6fb4c814593
danish0410 <thanigai_velan@yahoo.com>
2025-07-24 14:38:37 +0530

    Update StaticVars.groovy

--- src/org/staticlib/StaticVars.groovy
+++ src/org/staticlib/StaticVars.groovy
@@ -1,12 +1,16 @@
 package org.staticlib
 
 class StaticVars {
-    static final List<String> SUPPORTED_APP_TYPES = ['springboot', 'nginx', 'php']
+    static final List<String> APP_TYPES = ['springboot', 'nginx', 'php']
     static final Map<String, Integer> DEFAULT_PORTS = [
         springboot: 8080,
         nginx     : 80,
         php       : 9000
     ]
+
+    static List<String> getAppTypes() {
+        return APP_TYPES
+    }
 
     static String getDefaultPort(String appType) {
         return DEFAULT_PORTS.get(appType?.toLowerCase(), 'UNKNOWN')
